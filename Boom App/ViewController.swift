f533caddcbd7c68310c74cc2341c7892ada86755
Tyler Nielsen <tyler611@gmail.com>
2016-06-19 08:24:07 -0600

    Initial Commit

---
+++ Boom App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Boom App
+//
+//  Created by Tyler Nielsen on 6/19/16.
+//  Copyright © 2016 Tyler Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Boom AppTests/Boom_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Boom_AppTests.swift
+//  Boom AppTests
+//
+//  Created by Tyler Nielsen on 6/19/16.
+//  Copyright © 2016 Tyler Nielsen. All rights reserved.
+//
+
+import XCTest
+@testable import Boom_App
+
+class Boom_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
